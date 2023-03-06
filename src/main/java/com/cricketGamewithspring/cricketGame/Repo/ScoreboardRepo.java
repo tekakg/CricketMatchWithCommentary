ab3c0a564df8bb71af26234f99715e1e1f5eebc1
tekakg <123165039+tekakg@users.noreply.github.com>
2023-03-06 11:53:08 +0530

    Update ScoreboardRepo.java

--- src/main/java/com/cricketGamewithspring/cricketGame/Repo/ScoreboardRepo.java
+++ src/main/java/com/cricketGamewithspring/cricketGame/Repo/ScoreboardRepo.java
@@ -2,9 +2,10 @@ package com.cricketGamewithspring.cricketGame.Repo;
 
 import com.cricketGamewithspring.cricketGame.model.Scoreboard;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface ScoreboardRepo extends MongoRepository<Scoreboard, Integer> {
     Optional<Scoreboard> findByMatchId(int matchid);
     //In this repo I will store scoreboard
